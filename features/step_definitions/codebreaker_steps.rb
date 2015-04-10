6ba11a9df085e540ab9537b7a4780ae129dcfd79
Hashrocket Workstation <dev@hashrocket.com>
2015-04-10 02:14:45 -0400

    finished chapter 9, Marker marks correctly for duplicates

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -1,27 +1,24 @@
 Given /^I am not yet playing$/ do
 end
 
-Given(/^the secret code is "([^"]*)"$/) do |code|
-  @messenger = StringIO.new
-  game = Codebreaker::Game.new(@messenger)
-  game.start(code)
-end
-
 When /^I start a new game$/ do
   @messenger = StringIO.new
   game = Codebreaker::Game.new(@messenger)
   game.start('1234')
 end
 
-When(/^I guess "([^"]*)"$/) do |guess|
-  @messenger = StringIO.new
-  game = Codebreaker::Game.new(@messenger)
-  game.start('1234')
-  game.guess(guess)
-end
-
 Then /^I should see "([^"]*)"$/ do |message|
   expect(@messenger.string.split("\n")).to include(message)
+end
+
+Given(/^the secret code is "([^"]*)"$/) do |code|
+  @messenger = StringIO.new
+  @game = Codebreaker::Game.new(@messenger)
+  @game.start(code)
+end
+
+When(/^I guess "([^"]*)"$/) do |guess|
+  @game.guess(guess) || @messenger.puts('👋') 
 end
 
 Then(/^the mark should be "([^"]*)"$/) do |mark|
--- lib/codebreaker/marker.rb
+++ lib/codebreaker/marker.rb
@@ -8,14 +8,20 @@ class Marker
 	end
   end
   def number_match_count
-	(0..3).inject(0) do |count, index|
-	  count + (number_match?(index) ? 1 : 0)
-    end
+	total_match_count - exact_match_count
+  end
+  private
+  def total_match_count
+  	count = 0
+  	secret = @secret.split('')
+  	@guess.split('').inject(0) do |count, n|
+  		count + (delete_first(secret, n) ? 1 : 0)
+  	end
+  end
+  def delete_first(code, n)
+  	code.delete_at(code.index(n)) if code.index(n)
   end
   def exact_match?(index)
 	@secret[index] == @guess[index]
-  end
-  def number_match?(index)
-    @secret.include?(@guess[index]) && !exact_match?(index)
   end
 end
